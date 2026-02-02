166de7518b92da1bcf75a08a5fc571eb278d51b3
6504062610242salankosol <s6504062610242@email.kmutnb.ac.th>
2026-02-02 19:03:15 +0700

    7-4 Lab7no4.c Square of stars

---
+++ 7-4 Lab7no4.c Square of stars.cpp
@@ -0,0 +1,34 @@
+/*Commit 4>> Lab7no4.c: Square of stars*/
+#include<stdio.h>
+
+void printHead(int w);
+void printBody(int w, int h);
+
+int main() 
+{
+    int width, height;
+    scanf("%d %d", &width, &height);
+
+    printHead(width);
+    printBody(width, height);
+    printHead(width);
+}
+
+void printHead(int w)
+{
+    for(int i = 0; i < 2*w; i++)
+        printf("*");
+    printf("\n");
+}
+
+void printBody(int w, int h)
+{
+    for(int i = 0; i < h-2; i++)
+    {
+        printf("*");
+        for(int j = 0; j < 2*w-2; j++)
+            printf(" ");
+        printf("*\n");
+    }
+}
+
